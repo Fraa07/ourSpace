cfffd3354933d68a7f74c4b27d80884bfa9d81ac
Francesco Podavini <minipoda06@gmai.com>
2026-03-26 13:46:18 +0100

    Sistemato multiplayer

--- client.js
+++ client.js
@@ -22,8 +22,8 @@ function resize() {
 resize();
 window.addEventListener('resize', resize);
 
-let me = null;
-let people = []; // TODO riempire con i dati che arrivano dal server
+let people = {}; // TODO riempire con i dati che arrivano dal server
+let myId = null;
 
 const personW = 40;
 const personH = 120;
@@ -121,16 +121,23 @@ const drawRightBtn = createButton('>', () => {
     selectedCharacterIdx = mod(selectedCharacterIdx - 1, characterNames.length);
 });
 const drawOkBtn = createButton('ok', () => {
-    me = {
+    const initMessage = {
+        kind: "init",
+        character: characterNames[selectedCharacterIdx]
+    };
+
+    socket.send(JSON.stringify(initMessage));
+  /*  me = {
         x: 0,
         y: 0,
         speed: 5,
         character: characterNames[selectedCharacterIdx],
-    };
+    }; */
 }, { main: "#58a515" });
 
 
 function draw() {
+    const me = myId ? people[myId] : null;
     if (me) {
         // gestione movimento
         if (goingUp) me.y -= me.speed;
@@ -164,6 +171,10 @@ function draw() {
             ctx.rect(worldBounds.left, worldBounds.top, worldW, worldH);
             ctx.fillStyle = "#58a515";
             ctx.fill();
+
+            Object.values(people).forEach(p => {
+                drawPerson(p.x, p.y, personW, personH, p.character);
+            })
 
             if (me) drawPerson(me.x, me.y, personW, personH, me.character);
         ctx.restore();
@@ -210,7 +221,21 @@ function drawPerson(x, y, w, h, style) {
 
 const socket = new WebSocket(`ws://localhost:4242`);
 socket.addEventListener("message", async event => {
-    // TODO aggiornare lo stato in base ai messaggi del server
+    const message = JSON.parse(event.data);
+    if (message.kind === "reset") {
+        Object.entries(message.people).forEach(entry => {
+            const [ id,person] = entry;
+            if(id !== myId) {
+                people[id] = person;
+            }
+            else if(!people[myId]){
+                people[myId] = person;
+            }
+        });
+    }
+    else if (message.kind === "id") {
+        myId = message.id;
+    }
 });
 
 // TODO spostare la gestione dei movimenti sul server
@@ -231,6 +256,29 @@ document.addEventListener("keyup", (event) => {
     else if (event.code == "KeyS") goingDown = false;
     else if (event.code == "KeyD") goingRight = false;
 });
+
+let prevX = 0, prevY = 0;
+const EPSILON = 0.00001;
+setInterval(() => {
+    const me = myId ? people[myId] : null;  
+    if (me) {
+        const distX = Math.abs(me.x - prevX)
+        const distY = Math.abs(me.y - prevY)
+
+        if (distX > EPSILON || distY > EPSILON) {
+            prevX = me.x;
+            prevY = me.y;
+            const moveMessage = {
+                kind: "move",
+                x: me.x,
+                y: me.y
+            }
+
+            socket.send(JSON.stringify(moveMessage));
+        }
+    }
+
+}, 1000/20 );
 
 // gestione dello zoom
 const minZoom = 0.1, maxZoom = 4;
